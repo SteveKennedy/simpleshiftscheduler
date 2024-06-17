b4e8d75f7ff4b7dca8b782830a1711c933f755a1
Steve Kennedy <SteveKennedy2024@outlook.com>
2024-06-16 22:14:18 -0500

    Initial Commit

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'simpleshiftscheduler',
-  webDir: 'www'
-};
-
-export default config;
